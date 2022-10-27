3d414ff75b1d801bd4af803cf5bdfd46df3365e8
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-10-27 16:26:47 +0700

    add array to be seacrhed

--- Activity3/Program.cs
+++ Activity3/Program.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace Activity3
 {
-    internal class Program
+    class Program
     {
+        //array to be seacrhed
+        int[] arr = new int[20];
+        //number of element in the array
+        int n;
+        //get the number of element to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("enter the number of element in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 element.\n");
+            }
+            //accept array element
+            Console.WriteLine("");
+            Console.WriteLine("---------------------");
+            Console.WriteLine(" Enter Array Element ");
+            Console.WriteLine("---------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = int.Parse(s1);
+            }
+        }
         static void Main(string[] args)
         {
         }
