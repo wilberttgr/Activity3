90c015efe4fab4e57a9ea47273d039e9755233aa
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-10-27 17:00:49 +0700

    add algorithm linear search

--- Activity3/Program.cs
+++ Activity3/Program.cs
@@ -79,6 +79,35 @@ namespace Activity3
                 ch = char.Parse(Console.ReadLine().ToUpper());
             } while ((ch == 'y'));
         }
+
+        public void linearSearch()
+        {
+            char ch;
+            // search for the number of comparison 
+            int ctr;
+            do
+            {
+                //accept the number to be searched 
+                Console.Write("\nEnter the element you want to search: ");
+                int item = Convert.ToInt32((Console.ReadLine()));
+
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + " found st position" + (i + 1).ToString());
+                        break;
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array");
+                Console.WriteLine("\nNumber of comparison: " + ctr);
+                Console.WriteLine("\nContinue search (y/n): ");
+                ch = char.Parse(Console.ReadLine().ToUpper());
+            } while ((ch == 'y'));
+        }
         static void Main(string[] args)
         {
         }
